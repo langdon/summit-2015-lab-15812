c451c70c125f42aad04800cdf1a5ddf7cd24af33
langdon <langdon@fedoraproject.org>
2015-05-29 21:53:00 -0400

    lab3: updating answer script to reflect lab2 changes and new registry address

--- vagrant/answers/complete-lab3.sh
+++ vagrant/answers/complete-lab3.sh
@@ -6,17 +6,18 @@ cd ~/sync/lab3
 docker stop bigapp #a little safer then $(docker ps -ql)
 docker rm bigapp #a little safer then $(docker ps -ql)
 
-docker build -t mariadb mariadb/ -f Dockerfile.reference
-docker build -t wordpress wordpress/ -f Dockerfile.reference
+docker build -t mariadb -f mariadb/Dockerfile.reference mariadb/.
+docker build -t wordpress -f wordpress/Dockerfile.reference wordpress/.
 
-TARGET_IP=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
+echo -e '\nLABEL RUN docker run -d -v ${HOME}/mysql:/var/lib/mysql  --name NAME -e DBUSER=${DBUSER} -e DBPASS={$DBPASS} -e DBNAME=${DBNAME} -e NAME=NAME -e IMAGE=IMAGE IMAGE' >> mariadb/Dockerfile.reference
 
-echo -e '\nLABEL RUN docker run -d -v ${HOME}/mysql:/var/lib/mysql  --name NAME -e DBUSER=${DBUSER} -e DBPASS={$DBPASS} -e DBNAME=${DBNAME} -e NAME=NAME -e IMAGE=IMAGE IMAGE' >> mariadb/Dockerfile
+echo -e '\nLABEL RUN docker run -d -v ${HOME}/wordpress:/var/www/html -p 80:80 --link=mariadb:db --name NAME -e NAME=NAME -e IMAGE=IMAGE IMAGE' >> wordpress/Dockerfile.reference
 
-echo -e '\nLABEL RUN docker run -d -v ${HOME}/wordpress:/var/www/html -p 80:80 --link=mariadb:db --name NAME -e NAME=NAME -e IMAGE=IMAGE IMAGE' >> wordpress/Dockerfile
+docker build -t mariadb -f mariadb/Dockerfile.reference mariadb/.
+docker build -t wordpress -f wordpress/Dockerfile.reference wordpress/.
 
-docker build -t mariadb mariadb/ -f Dockerfile.reference
-docker build -t wordpress wordpress/ -f Dockerfile.reference
+#TARGET_IP=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
+TARGET_IP=summit-rhel-dev
 
 docker tag mariadb $TARGET_IP:5000/mariadb
 docker tag wordpress $TARGET_IP:5000/wordpress
