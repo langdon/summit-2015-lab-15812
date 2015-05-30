b092195951f8deb466893b5898b4e8f80e0842bb
langdon <langdon@fedoraproject.org>
2015-05-29 21:41:13 -0400

    lab2: updating answer shell script to reflect container/image name changes

--- vagrant/answers/complete-lab2.sh
+++ vagrant/answers/complete-lab2.sh
@@ -2,5 +2,5 @@
 #execute the functionality in lab2 that other labs depend on 
 
 cd ~/sync/lab2/bigapp
-docker build -t monolithic .
-docker run -p 80 --name=bigapp -e DBUSER=user -e DBPASS=mypassword -e DBNAME=mydb -d monolithic
+docker build -t bigimg .
+docker run -p 80 --name=bigapp -e DBUSER=user -e DBPASS=mypassword -e DBNAME=mydb -d bigimg
