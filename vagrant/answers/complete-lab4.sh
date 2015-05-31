3cb5a189d2e3abd7cabfd222377b714c5020d929
langdon <langdon@fedoraproject.org>
2015-05-30 21:55:30 -0400

    probably not quite right yet, but a pass at a lab4 answer script

--- vagrant/answers/complete-lab4.sh
+++ vagrant/answers/complete-lab4.sh
@@ -3,6 +3,8 @@
 
 cd ~/lab4
 
+echo "make sure your /etc/hosts file has the two servers listed"
+
 TARGET_IP=summit-rhel-dev:5000
 SED_REGEXP="s/MY_REPOSITORY_WITH_IMAGE_NAME/$TARGET_IP/g"
 
@@ -10,4 +12,21 @@ cp -R ~/lab4/* ~/workspace
 
 sed -i -e "s/MY_REPOSITORY_WITH_IMAGE_NAME/$TARGET_IP\/wordpress/g" ~/workspace/wordpress/kubernetes/wordpress-pod.json
 sed -i -e "s/MY_REPOSITORY_WITH_IMAGE_NAME/$TARGET_IP\/mariadb/g" ~/workspace/mariadb/kubernetes/mariadb-pod.json 
+
+mkdir ~/.kube
+touch ~/.kube/.kubeconfig
+
+kubectl config set-cluster local --server=http://localhost:8080
+kubectl config set-context local-context --cluster=local
+kubectl config use-context local-context
+
+kubectl create -f ~/workspace/mariadb/kubernetes/mariadb-pod.json &&  kubectl create -f ~/workspace/mariadb/kubernetes/mariadb-service.json 
+kubectl create -f ~/workspace/wordpress/kubernetes/wordpress-pod.json &&  kubectl create -f ~/workspace/wordpress/kubernetes/wordpress-service.json 
+  
+kubectl config set-cluster remote --server=summit_rhel_deploy_target:8080 
+kubectl config set-context remote-context --cluster=remote
+kubectl config use-context remote-context
+
+kubectl create -f ~/workspace/mariadb/kubernetes/mariadb-pod.json &&  kubectl create -f ~/workspace/mariadb/kubernetes/mariadb-service.json 
+kubectl create -f ~/workspace/wordpress/kubernetes/wordpress-pod.json &&  kubectl create -f ~/workspace/wordpress/kubernetes/wordpress-service.json 
 
