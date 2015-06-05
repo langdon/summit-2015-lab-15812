145f5f75bc169a554c1cf7e8c546f07d2d9511f7
langdon <langdon@fedoraproject.org>
2015-06-05 07:36:52 -0400

    fix up for qcow2 v3; commenting out move commands

--- convert-vagrant-to-libvirt/convert-from-vagrant.sh
+++ convert-vagrant-to-libvirt/convert-from-vagrant.sh
@@ -21,18 +21,18 @@ virsh dumpxml $NAME_OF_DEPLOY > summit_rhel_deploy_target.xml
 #convert to qcow, rebase image for dev machine
 IMAGE=`sed -e "s/.*'\(.*summit_rhel_dev.img\)'.*/\1/g" summit_rhel_dev.xml | grep img`
 echo "Dev image name: $IMAGE; about to convert, might be awhile"
-qemu-img convert -O qcow2 $IMAGE ./summit_rhel_dev.qcow2
-mv ./summit_rhel_dev.qcow2 /var/lib/libvirt/images/
-echo "moving ./summit_rhel_dev.qcow2 /var/lib/libvirt/images/"
-chown qemu:qemu /var/lib/libvirt/images/summit_rhel_dev.qcow2
+qemu-img convert -f qcow2 -O qcow2 -o compat=0.10 $IMAGE ./summit_rhel_dev.qcow2
+#mv ./summit_rhel_dev.qcow2 /var/lib/libvirt/images/
+#echo "moving ./summit_rhel_dev.qcow2 /var/lib/libvirt/images/"
+#chown qemu:qemu /var/lib/libvirt/images/summit_rhel_dev.qcow2
 
 #convert to qcow, rebase image for deploy machine
 IMAGE=`sed -e "s/.*'\(.*summit_rhel_deploy_target.img\)'.*/\1/g" summit_rhel_deploy_target.xml | grep img`
 echo "Deploy image name: $IMAGE; about to convert, might be awhile"
-qemu-img convert -O qcow2 $IMAGE ./summit_rhel_deploy_target.qcow2
-echo "moving ./summit_rhel_deploy_target.qcow2 /var/lib/libvirt/images/"
-mv ./summit_rhel_deploy_target.qcow2 /var/lib/libvirt/images/
-chown qemu:qemu /var/lib/libvirt/images/summit_rhel_deploy_target.qcow2
+qemu-img convert -f qcow2 -O qcow2 -o compat=0.10 $IMAGE ./summit_rhel_deploy_target.qcow2
+#echo "moving ./summit_rhel_deploy_target.qcow2 /var/lib/libvirt/images/"
+#mv ./summit_rhel_deploy_target.qcow2 /var/lib/libvirt/images/
+#chown qemu:qemu /var/lib/libvirt/images/summit_rhel_deploy_target.qcow2
 
 echo "fix up the xml to give some prettier names"
 sed -i -e "s|<name>.*</name>|<name>summit_rhel_dev</name>|g" summit_rhel_dev.xml
@@ -42,10 +42,11 @@ sed -i -e "s|<uuid>.*</uuid>|<uuid>`uuidgen`</uuid>|g" summit_rhel_deploy_target
 
 echo "fix up the xml to point to this new file"
 echo "this may be wrong if you dont use the default pool for your vms"
-sed -i -e "s|'\(.*summit_rhel_dev.img\)'|'/var/lib/libvirt/images/summit_rhel_dev.qcow2'|g" summit_rhel_dev.xml
-sed -i -e "s|'\(.*summit_rhel_deploy_target.img\)'|'/var/lib/libvirt/images/summit_rhel_deploy_target.qcow2'|g" summit_rhel_deploy_target.xml
+sed -i -e "s|'\(.*summit_rhel_dev.img\)'|'./summit_rhel_dev.qcow2'|g" summit_rhel_dev.xml
+sed -i -e "s|'\(.*summit_rhel_deploy_target.img\)'|'./summit_rhel_deploy_target.qcow2'|g" summit_rhel_deploy_target.xml
 
 echo "fix up the xml to point to the default network"
 echo "this may be wrong if you dont use the default network for your vms"
 sed -i -e "s|<source network='vagrant-libvirt'/>|<source network='default'/>|g" summit_rhel_dev.xml
 sed -i -e "s|<source network='vagrant-libvirt'/>|<source network='default'/>|g" summit_rhel_deploy_target.xml
+
